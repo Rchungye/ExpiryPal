b27670189e038b50d7776a6b1242581d06c377ac
Rafael Chung <rafaelchung28@hotmail.com>
2024-12-05 17:08:53 -0500

    requierements update

--- be/src/__init__.py
+++ be/src/__init__.py
@@ -1,24 +1,22 @@
-from functools import wraps
 import os
-from flask import Flask, Blueprint
+from flask import Flask
 from src.config import config
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 
+
 APP_ROOT = os.path.join(os.path.dirname(__file__), "..")
 dotenv_path = os.path.join(APP_ROOT, ".env")
 load_dotenv(dotenv_path)
-
 app = Flask(__name__)
-
 enviroment = config["envConfig"]
 app.config.from_object(enviroment)
 db = SQLAlchemy(app)
-
 Migrate(app, db)
 CORS(app, supports_credentials=True)
+
 
 from src.routes.camera_routes import *
 from src.routes.fridge_routes import *
