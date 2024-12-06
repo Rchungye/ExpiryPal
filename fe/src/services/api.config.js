9cd9edf681d6449cab3623e08085af417fbc434c
Rafael Chung <rafaelchung28@hotmail.com>
2024-12-05 22:26:54 -0500

    Update api.config.js

--- fe/src/services/api.config.js
+++ fe/src/services/api.config.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const ApiService = axios.create({
-    // baseURL: mport.meta.env.VITE_BE_URL, // // // BACKEND URL // // // 
-    baseURL: 'http://127.0.0.1:5001/', // // // BACKEND URL // // // 
+    baseURL: mport.meta.env.VITE_BE_URL, // // // BACKEND URL // // // 
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
