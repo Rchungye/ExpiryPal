9bc9a625f3f8fb2f97160a389c5f0cb008fcacbc
Rafael Chung <rafaelchung28@hotmail.com>
2024-12-09 14:54:57 -0500

    Add: Flask-APPScheduler

--- be/src/routes/camera_routes.py
+++ be/src/routes/camera_routes.py
@@ -1,8 +1,9 @@
-from src import app
+from src import app, scheduler
 import json
 from flask import jsonify, request
 from src.controllers import (
-    CamaraController as Camera
+    CamaraController as Camera,
+    HASSController as HASS
 )
 
 
@@ -10,4 +11,41 @@ from src.controllers import (
 def GetAllCameras():
     result = Camera.GetAllCameras()
     return result.jsonify()
+
+
+@app.route("/obtener_url_camara", methods=["GET"])
+def obtener_url_camara_route():
+    """
+    Ruta para obtener la URL de la cámara desde Home Assistant.
+
+    Query Params:
+        - entity_id (str): ID de la entidad de la cámara.
+        - base_url (str): URL base del servidor de Home Assistant.
+
+    Returns:
+        JSON con el estado de la operación y la URL de la cámara.
+    """
+    entity_id = request.args.get("entity_id")
+    base_url = request.args.get("base_url")
+    
+    if not entity_id or not base_url:
+        return jsonify({"status": 400, "mensaje": "Faltan parámetros necesarios."})
+    
+    resultado = HASS.obtener_url_camara(entity_id, base_url)
+    return jsonify(resultado)
+
+
+@scheduler.task("interval", id="actualizar_url_camara", minutes=10)
+def actualizar_url_camara_task():
+    """
+    Tarea programada que obtiene e imprime la URL de la cámara.
+    """
+    entity_id = "camera.192_168_8_125"
+    base_url = "https://hass.mdu-smartroom.se"
+    resultado = HASS.obtener_url_camara(entity_id, base_url)
+
+    if resultado["status"] == 200:
+        print(f"URL de la cámara obtenida: {resultado['url']}")
+    else:
+        print(f"Error: {resultado['mensaje']}")
 
---
+++ fe/src/services/Cloudinary.config.js
@@ -0,0 +1,10 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+// Configuración de Cloudinary
+cloudinary.config({
+  cloud_name: 'dcctrhgob',
+  api_key: 'dasdasdasdas',
+  api_secret: 'dasdasdasd'
+});
+
+export { cloudinary };
