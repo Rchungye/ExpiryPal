e1292ded42800f80a65d545baed5eee361b21299
Rafael Chung <rafaelchung28@hotmail.com>
2024-12-15 16:25:00 -0500

    ItemServices

--- fe/src/services/notificationService.js
+++ fe/src/services/notificationService.js
@@ -1,34 +1,3 @@
-// // src/services/notificationPreferences.js
-// const BASE_URL = "http://192.168.92.67:5001/notificationPreferences";
-
-// /**
-//  * Guarda las preferencias de notificaciones.
-//  * @param {Object} payload - Datos para guardar las preferencias.
-//  * @returns {Promise<Response>} Respuesta de la API.
-//  */
-// export const saveNotificationPreferences = async (payload) => {
-//   try {
-//     const response = await fetch(BASE_URL, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(payload),
-//     });
-
-//     if (!response.ok) {
-//       throw new Error(`Error: ${response.statusText}`);
-//     }
-
-//     return response.json();
-//   } catch (error) {
-//     console.error("Error saving notification preferences:", error);
-//     throw error;
-//   }
-// };
-
-
-
 import { ApiService } from './api.config';
 
 // // // // // NOTIFICATION PREFERENCES // // // // //
