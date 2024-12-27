ad42c183624b80760547e3d12d447db45a3f599f
Victor <victor.rodriguez23@utp.ac.pa>
2024-12-27 09:08:30 -0500

    a

--- be/app.py
+++ be/app.py
@@ -2,12 +2,7 @@
 from src import app
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    app.run(host='0.0.0.0', port='5001', debug=False) # DEV
-    # app.run(host='0.0.0.0', debug=False) # PROD
-=======
     app.run(host='0.0.0.0', port='5001', debug=True) # DEV
     # app.run(host='0.0.0.0', debug=False) # PROD
 
     
->>>>>>> 21f85a478ec0c56435798f535a64937f40b32d92
